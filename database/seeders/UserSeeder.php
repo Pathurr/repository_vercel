0f2d0988767d6ae8123bbeb1d2bb7a3204bfbc28
Pathurr <alfathurahman28@gmail.com>
2026-07-29 07:19:10 +0700

    Add personal accounts to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,13 +11,21 @@ class UserSeeder extends Seeder
     {
         // Admin
         User::create([
-            'name' => 'Admin',
-            'email' => 'admin@lms.com',
+            'name' => 'Admin Utama',
+            'email' => 'admin@admin.com',
             'password' => 'password',
             'role' => 'admin',
         ]);
 
         // Guru
+        User::create([
+            'name' => 'Still Goodman',
+            'email' => 'stillgoodman466@gmail.com',
+            'password' => 'goodman46',
+            'role' => 'guru',
+            'nrg' => '1122334455',
+        ]);
+
         User::create([
             'name' => 'Budi Santoso',
             'email' => 'guru1@lms.com',
@@ -36,18 +44,19 @@ class UserSeeder extends Seeder
 
         // Siswa
         $siswa = [
-            ['name' => 'Andi Pratama',   'email' => 'siswa1@lms.com', 'nis' => '2024001'],
-            ['name' => 'Dewi Lestari',   'email' => 'siswa2@lms.com', 'nis' => '2024002'],
-            ['name' => 'Rizky Ramadan',  'email' => 'siswa3@lms.com', 'nis' => '2024003'],
-            ['name' => 'Aulia Putri',    'email' => 'siswa4@lms.com', 'nis' => '2024004'],
-            ['name' => 'Fajar Nugroho',  'email' => 'siswa5@lms.com', 'nis' => '2024005'],
+            ['name' => 'Alfathurahman',  'email' => 'alfathurahman28@gmail.com', 'nis' => '2024000', 'password' => 'dompak28jb'],
+            ['name' => 'Andi Pratama',   'email' => 'siswa1@lms.com', 'nis' => '2024001', 'password' => 'password'],
+            ['name' => 'Dewi Lestari',   'email' => 'siswa2@lms.com', 'nis' => '2024002', 'password' => 'password'],
+            ['name' => 'Rizky Ramadan',  'email' => 'siswa3@lms.com', 'nis' => '2024003', 'password' => 'password'],
+            ['name' => 'Aulia Putri',    'email' => 'siswa4@lms.com', 'nis' => '2024004', 'password' => 'password'],
+            ['name' => 'Fajar Nugroho',  'email' => 'siswa5@lms.com', 'nis' => '2024005', 'password' => 'password'],
         ];
 
         foreach ($siswa as $s) {
             User::create([
                 'name' => $s['name'],
                 'email' => $s['email'],
-                'password' => 'password',
+                'password' => $s['password'],
                 'role' => 'murid',
                 'nis' => $s['nis'],
             ]);
